50f211fa1f968797ecb9875a6da6a6252c6ecf4a
stefon <s.j.simmons@spartans.nsu.edu>
2019-12-14 22:52:39 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,17 @@
-console.log("Yeeeeeaaahh Boiii");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("User flipped " + cardsInPlay);
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else{
+        alert("Sorry, try again");
+    }
+}
