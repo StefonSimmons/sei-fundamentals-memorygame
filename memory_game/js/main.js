41b574c87af7b433d1e6011b1ac2367b52fe8ba2
stefon <s.j.simmons@spartans.nsu.edu>
2019-12-18 21:43:21 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        name: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png" 
+    },
+    {
+        name: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"   
+    },
+    {
+        name: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        name: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 function checkforMatch(){
@@ -11,13 +32,16 @@ function checkforMatch(){
 }
 
 function flipCard(cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].name);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+
+    cardsInPlay.push(cards[cardId].name);
     if(cardsInPlay.length === 2){
         checkforMatch();
     }    
 }
 
-flipCard(3);
-flipCard(2);
+flipCard(0);
+flipCard(1);
 console.log(cardsInPlay);
